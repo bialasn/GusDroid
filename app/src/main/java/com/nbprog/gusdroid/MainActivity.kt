fec7597a04192b1c12d9b7a08323ad30c8f9f101
mbogutczak <Mikolaj.Bogutczak@Harman.com>
2021-11-18 22:24:04 +0100

    Add example of error handling, fix getByNip

--- app/src/main/java/com/nbprog/gusdroid/MainActivity.kt
+++ app/src/main/java/com/nbprog/gusdroid/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.whenCreated
 import com.nbprog.gusdroid.gus.Gus
+import com.nbprog.gusdroid.model.GusResult
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,13 @@ class MainActivity : AppCompatActivity() {
             val result = Gus.authenticate("b0663cba094245d8802b")
             val info = Gus.getInfoBy(nip = "7681841967")
             Log.i("GUS",info.toString())
-            info
+            //TODO: there is necessary to check in GusResult.Success if authorization e.x is null
+            when(info){
+                is GusResult.GenericError -> { }
+                GusResult.NetworkError -> { }
+                is GusResult.ParsingError -> { }
+                is GusResult.Success -> { }
+            }
         }
     }
 }
--- gusdroid/src/main/java/com/nbprog/gusdroid/gus/methods/getinfobyparameter/GetInfoByParameterRequestBuilder.kt
+++ gusdroid/src/main/java/com/nbprog/gusdroid/gus/methods/getinfobyparameter/GetInfoByParameterRequestBuilder.kt
@@ -19,34 +19,42 @@ class GetInfoByParameterRequestBuilder : RequestBuilder {
                 <soap:Body>
                 <ns:DaneSzukajPodmioty>
                 <ns:pParametryWyszukiwania>
-                <!--Optional:-->
-                <dat:Regon></dat:Regon>
-                <!--Optional:-->
-                <dat:Nip></dat:Nip>
-                <!--Optional:-->
-                <dat:Krs></dat:Krs>
-                <!--Optional:-->
-                <dat:Nipy></dat:Nipy>
-                <!--Optional:-->
-                <dat:Regony9zn></dat:Regony9zn>
-                <!--Optional:-->
-                <dat:Krsy></dat:Krsy>
-                <!--Optional:-->
-                <dat:Regony14zn></dat:Regony14zn>
                 </ns:pParametryWyszukiwania>
                 </ns:DaneSzukajPodmioty>
                 </soap:Body>
                 </soap:Envelope>"""
 
     override fun createRequest(parameters: Map<String, String?>) : String{
-        var requestCreator = request
-
+        var createdRequestWithParameters = request
         parameters.forEach { (paramName,paramValue) ->
-            paramValue?.let {
-                val paramCloseTag = paramName.replace("<","</")
-                requestCreator = request.replace("$paramName$paramCloseTag","$paramName$it$paramCloseTag")
+            if(paramValue != null){
+                createdRequestWithParameters = createdRequestWithParameters
+                    .addParamBetween("<ns:pParametryWyszukiwania>",paramName,paramValue.orEmpty())
             }
         }
-        return requestCreator
+        return createdRequestWithParameters
     }
+
+    private fun String.addParamBetween(tag: String, tagToAdd: String, value: String): String {
+        val paramCloseTag = tagToAdd.replace("<", "</")
+        val splittedByOpenTag = this.split(tag)
+
+        return "${splittedByOpenTag[0]}$tag\n$tagToAdd$value$paramCloseTag${splittedByOpenTag[1]}"
+    }
+
+    //TODO: This is all optional parameter, only Regon and nip are handled
+//    <!--Optional:-->
+//    <dat:Regon></dat:Regon>
+//    <!--Optional:-->
+//    <dat:Nip></dat:Nip>
+//    <!--Optional:-->
+//    <dat:Krs></dat:Krs>
+//    <!--Optional:-->
+//    <dat:Nipy></dat:Nipy>
+//    <!--Optional:-->
+//    <dat:Regony9zn></dat:Regony9zn>
+//    <!--Optional:-->
+//    <dat:Krsy></dat:Krsy>
+//    <!--Optional:-->
+//    <dat:Regony14zn></dat:Regony14zn>
 }
